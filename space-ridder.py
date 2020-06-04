0f6f723b5ff780a285a6fef791f77523a61fcfa3
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-04 14:50:07 +0300

    fix or

--- space-ridder.py
+++ space-ridder.py
@@ -3,7 +3,7 @@ file_name = input("enter a valid file name for the .txt file that will be create
 
 formatted = ""
 for i in range(0, len(text)):
-	if(text[i] != '\n' || (text[i + 1] == '\n')):
+	if(text[i] != '\n' or (text[i + 1] == '\n')):
 		formatted += char
 		continue
 
