1b0af83d454ae1445648224b2d082f638230a619
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-04 14:48:05 +0300

    create the script

---
+++ space-ridder.py
@@ -0,0 +1,14 @@
+text = input("enter the text you want formatted: ")
+file_name = input("enter a valid file name for the .txt file that will be created to host formatted output: ")
+
+formatted = ""
+for i in range(0, len(text)):
+	if(text[i] != '\n' || (text[i + 1] == '\n')):
+		formatted += char
+		continue
+
+f = open(file_name, "w")
+f.write(formatted)
+f.close()
+
+print("Your file has been created! Have a nice day :)")
