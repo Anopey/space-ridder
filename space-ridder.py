a33b0bf53e79f14010b58ad9e5f3f20287aa87c6
Ozan Ürkmez <arkantos250@gmail.com>
2020-06-04 15:05:16 +0300

    fix things up and become more performant

--- space-ridder.py
+++ space-ridder.py
@@ -1,13 +1,21 @@
-text = input("enter the text you want formatted: ")
-file_name = input("enter a valid file name for the .txt file that will be created to host formatted output: ")
+print("make sure to enter the text you want formatted into input.txt!\n")
+file_name = input("enter a valid file name for the .txt file that will be created to host formatted output. Do so without the .txt extension: ")
+
+f= open("input.txt", "r")
+text = f.read()
 
 formatted = ""
-for i in range(0, len(text)):
+lengthminus = len(text) - 1
+for i in range(0, lengthminus):
 	if(text[i] != '\n' or (text[i + 1] == '\n')):
-		formatted += char
+		formatted += text[i]
 		continue
+	formatted += ' '
+#now for last index without losing performance
+if(text[lengthminus] != '\n'):
+	formatted += text[lengthminus]
 
-f = open(file_name, "w")
+f = open("output/" + file_name + ".txt", "w")
 f.write(formatted)
 f.close()
 
